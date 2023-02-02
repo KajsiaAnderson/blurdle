37663326aac6e662b7ac6838410a6c7d09cf920a
Kajsia Anderson <kajsia.anderson@gmail.com>
2023-02-02 16:36:31 -0700

    updated input field with mui

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,25 +1,78 @@
-import React, {useContext, useRef} from 'react'
-import { FiSearch } from 'react-icons/fi'
+import React, { useState, useContext } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { styled } from "@mui/material/styles";
+import { Paper } from '@mui/material'
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button"
 import { AppContext } from '../App'
 
 
-const Submit = () => {
-    const inputRef = useRef()
-    const {board, setBoard, currAttempt, setCurrAttempt} = useContext(AppContext)
+const StyledAutocomplete = styled(Autocomplete)({
+    "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
+        paddingLeft: 20
+    },
+    "& .MuiInputLabel-outlined": {
+        color: "gray"
+    },
+    "&.Mui-focused .MuiInputLabel-outlined": {
+        color: "gray"
+    },
+    "& .MuiAutocomplete-inputRoot": {
+        color: "gray",
+        '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-of-type': {
+            paddingLeft: 20
+        },
+        "& .MuiAutocomplete-input": {
+            borderColor: "gray"
+        },
+        "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "gray"
+        },
+        "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "white"
+        },
+        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "gray"
+        }
+    },
+    "& .MuiSvgIcon-root": {
+        color: "white"
+    },
+});
 
-    const checkRef = (e) => {
-        e.preventDefault()
+
+export default function Submit() {
+    const { board, setBoard, currAttempt, setCurrAttempt, correctMovie } = useContext(AppContext)
+
+    const [value, setValue] = useState(null);
+
+    // const handleEnter = (e) => { if (e.key === 'Enter') { console.log(e.target.value); } }
+
+    const handleSubmit = (event, stateVal) => {
+        event.preventDefault();
+        console.log(event);
+        console.log(stateVal);
+        console.log(stateVal.label);
+
         if (currAttempt.attempt > 5) return;
-        if (inputRef.current.value === '') return;
+        if (stateVal.label === '') return;
 
         const newBoard = [...board]
-        newBoard[currAttempt.attempt] = inputRef.current.value
+        newBoard[currAttempt.attempt] = stateVal.label
+        console.log('e', newBoard)
+
+
         setBoard(newBoard)
-        setCurrAttempt({...currAttempt, attempt: currAttempt.attempt + 1})
+        setCurrAttempt({ ...currAttempt, attempt: currAttempt.attempt + 1 })
         console.log('newBoard', newBoard)
-        inputRef.current.value = ''
-        inputRef.current.focus()
-    }
+        setValue(null)
+
+
+        if (newBoard[currAttempt.attempt] === correctMovie) {
+            alert('you won')
+        }
+    };
 
     const skipHandler = (e) => {
         e.preventDefault()
@@ -28,24 +81,195 @@ const Submit = () => {
         const newBoard = [...board]
         newBoard[currAttempt.attempt] = 'skipped'
         setBoard(newBoard)
-        setCurrAttempt({...currAttempt, attempt: currAttempt.attempt + 1})
+        setCurrAttempt({ ...currAttempt, attempt: currAttempt.attempt + 1 })
         console.log('skipBoard', newBoard)
-        inputRef.current.value = ''
-        inputRef.current.focus()
+        setValue(null)
     }
 
     return (
-        <form className='guess-section' onSubmit={checkRef}>
-            <div className='guess'>
-                <FiSearch className='search' color="#444" size="1.25rem" />
-                <input type="text" placeholder="Know it? Enter your guess here" ref={inputRef}/>
-            </div>
-            <div className='guess-btns'>
-                <button className='skip-btn' type='button' onClick={skipHandler}>Skip</button>
-                <button className='submit-btn' type='submit'>Submit</button>
-            </div>
+        <form onSubmit={(event) => handleSubmit(event, value)} className="guess-section">
+            <StyledAutocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                value={value}
+                onChange={(event, value) => setValue(value)}
+                clearOnBlur={true}
+                PaperComponent={({ children }) => (
+                    <Paper style={{ background: "#444", color: "white" }}>{children}</Paper>
+                )}
+                sx={{ width: 640 }}
+                renderInput={(params) => <TextField
+                    className='textField'
+                    // onKeyDown={(e) => handleEnter(e)}
+                    {...params}
+                    label="Guess the movie"
+                    sx={{ input: { color: 'white' } }} />}
+            />
+            <Stack display="flex" justifyContent="space-between" direction="row">
+                <Button type="button" variant="outlined" onClick={skipHandler}
+                    sx={{
+                        borderRadius: 50,
+                        color: "white",
+                        borderColor: '#444',
+                        margin: 1.5,
+                        paddingInline: 4,
+                        paddingBlock: 1,
+                        transition: "transform 0.15s ease-in-out",
+                        '&:hover': {
+                            transform: "scale3d(1.05, 1.05, 1)",
+                            backgroundColor: 'transparent',
+                            borderColor: "white"
+                        }
+                    }}>
+                    Skip
+                </Button>
+                <Button type="submit" variant="contained"
+                    sx={{
+                        borderRadius: 50,
+                        color: "black",
+                        backgroundColor: "#F5C518",
+                        margin: 1.5,
+                        paddingInline: 3,
+                        paddingBlock: 1,
+                        transition: "transform 0.15s ease-in-out",
+                        '&:hover': {
+                            transform: "scale3d(1.05, 1.05, 1)",
+                            backgroundColor: '#F5C518'
+                        },
+                    }}>
+                    Submit
+                </Button>
+            </Stack>
         </form>
-    )
+    );
 }
 
-export default Submit
+
+
+const top100Films = [
+    { label: 'The Shawshank Redemption', year: 1994 },
+    { label: 'The Godfather', year: 1972 },
+    { label: 'The Godfather: Part II', year: 1974 },
+    { label: 'The Dark Knight', year: 2008 },
+    { label: '12 Angry Men', year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: 'Pulp Fiction', year: 1994 },
+    {
+        label: 'The Lord of the Rings: The Return of the King',
+        year: 2003,
+    },
+    { label: 'The Good, the Bad and the Ugly', year: 1966 },
+    { label: 'Fight Club', year: 1999 },
+    {
+        label: 'The Lord of the Rings: The Fellowship of the Ring',
+        year: 2001,
+    },
+    {
+        label: 'Star Wars: Episode V - The Empire Strikes Back',
+        year: 1980,
+    },
+    { label: 'Forrest Gump', year: 1994 },
+    { label: 'Inception', year: 2010 },
+    {
+        label: 'The Lord of the Rings: The Two Towers',
+        year: 2002,
+    },
+    { label: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { label: 'Goodfellas', year: 1990 },
+    { label: 'The Matrix', year: 1999 },
+    { label: 'Seven Samurai', year: 1954 },
+    {
+        label: 'Star Wars: Episode IV - A New Hope',
+        year: 1977,
+    },
+    { label: 'City of God', year: 2002 },
+    { label: 'Se7en', year: 1995 },
+    { label: 'The Silence of the Lambs', year: 1991 },
+    { label: "It's a Wonderful Life", year: 1946 },
+    { label: 'Life Is Beautiful', year: 1997 },
+    { label: 'The Usual Suspects', year: 1995 },
+    { label: 'Léon: The Professional', year: 1994 },
+    { label: 'Spirited Away', year: 2001 },
+    { label: 'Saving Private Ryan', year: 1998 },
+    { label: 'Once Upon a Time in the West', year: 1968 },
+    { label: 'American History X', year: 1998 },
+    { label: 'Interstellar', year: 2014 },
+    { label: 'Casablanca', year: 1942 },
+    { label: 'City Lights', year: 1931 },
+    { label: 'Psycho', year: 1960 },
+    { label: 'The Green Mile', year: 1999 },
+    { label: 'The Intouchables', year: 2011 },
+    { label: 'Modern Times', year: 1936 },
+    { label: 'Raiders of the Lost Ark', year: 1981 },
+    { label: 'Rear Window', year: 1954 },
+    { label: 'The Pianist', year: 2002 },
+    { label: 'The Departed', year: 2006 },
+    { label: 'Terminator 2: Judgment Day', year: 1991 },
+    { label: 'Back to the Future', year: 1985 },
+    { label: 'Whiplash', year: 2014 },
+    { label: 'Gladiator', year: 2000 },
+    { label: 'Memento', year: 2000 },
+    { label: 'The Prestige', year: 2006 },
+    { label: 'The Lion King', year: 1994 },
+    { label: 'Apocalypse Now', year: 1979 },
+    { label: 'Alien', year: 1979 },
+    { label: 'Sunset Boulevard', year: 1950 },
+    {
+        label: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+        year: 1964,
+    },
+    { label: 'The Great Dictator', year: 1940 },
+    { label: 'Cinema Paradiso', year: 1988 },
+    { label: 'The Lives of Others', year: 2006 },
+    { label: 'Grave of the Fireflies', year: 1988 },
+    { label: 'Paths of Glory', year: 1957 },
+    { label: 'Django Unchained', year: 2012 },
+    { label: 'The Shining', year: 1980 },
+    { label: 'WALL·E', year: 2008 },
+    { label: 'American Beauty', year: 1999 },
+    { label: 'The Dark Knight Rises', year: 2012 },
+    { label: 'Princess Mononoke', year: 1997 },
+    { label: 'Aliens', year: 1986 },
+    { label: 'Oldboy', year: 2003 },
+    { label: 'Once Upon a Time in America', year: 1984 },
+    { label: 'Witness for the Prosecution', year: 1957 },
+    { label: 'Das Boot', year: 1981 },
+    { label: 'Citizen Kane', year: 1941 },
+    { label: 'North by Northwest', year: 1959 },
+    { label: 'Vertigo', year: 1958 },
+    {
+        label: 'Star Wars: Episode VI - Return of the Jedi',
+        year: 1983,
+    },
+    { label: 'Reservoir Dogs', year: 1992 },
+    { label: 'Braveheart', year: 1995 },
+    { label: 'M', year: 1931 },
+    { label: 'Requiem for a Dream', year: 2000 },
+    { label: 'Amélie', year: 2001 },
+    { label: 'A Clockwork Orange', year: 1971 },
+    { label: 'Like Stars on Earth', year: 2007 },
+    { label: 'Taxi Driver', year: 1976 },
+    { label: 'Lawrence of Arabia', year: 1962 },
+    { label: 'Double Indemnity', year: 1944 },
+    {
+        label: 'Eternal Sunshine of the Spotless Mind',
+        year: 2004,
+    },
+    { label: 'Amadeus', year: 1984 },
+    { label: 'To Kill a Mockingbird', year: 1962 },
+    { label: 'Toy Story 3', year: 2010 },
+    { label: 'Logan', year: 2017 },
+    { label: 'Full Metal Jacket', year: 1987 },
+    { label: 'Dangal', year: 2016 },
+    { label: 'The Sting', year: 1973 },
+    { label: '2001: A Space Odyssey', year: 1968 },
+    { label: "Singin' in the Rain", year: 1952 },
+    { label: 'Toy Story', year: 1995 },
+    { label: 'Bicycle Thieves', year: 1948 },
+    { label: 'The Kid', year: 1921 },
+    { label: 'Inglourious Basterds', year: 2009 },
+    { label: 'Snatch', year: 2000 },
+    { label: '3 Idiots', year: 2009 },
+    { label: 'Monty Python and the Holy Grail', year: 1975 },
+];
