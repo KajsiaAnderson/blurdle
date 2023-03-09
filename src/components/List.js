d9f42eae2bf069e84c3ca26a3fb87e1503215d39
Kajsia Anderson <kajsia.anderson@gmail.com>
2023-03-09 14:17:06 -0700

    added blur effect to image

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { AppContext } from '../App'
 import Guess from './Guess'
+import Blur from "react-blur"
 
 const List = () => {
+    const { currAttempt } = useContext(AppContext)
+    const [blur, setBlur] = useState(14)
+
+    useEffect(() => {
+        if (currAttempt.attempt === 0) {
+            setBlur(14)
+        } else if (currAttempt.attempt === 1) {
+            setBlur(11)
+        } else if (currAttempt.attempt === 2) {
+            setBlur(8)
+        } else if (currAttempt.attempt === 3) {
+            setBlur(5)
+        } else if (currAttempt.attempt === 4) {
+            setBlur(2)
+        } else {
+            setBlur(0)
+        }
+    }, [currAttempt])
 
     return (
         <div className='guess-list'>
@@ -24,8 +44,8 @@ const List = () => {
             <div className='guess-box'>
                 <Guess attempt={5} />
             </div>
-            <div>
-                <img src="https://filmschoolrejects.com/wp-content/uploads/2020/05/inception-top.jpg" alt="movie scene" />
+            <div className='img'>
+                <Blur img="https://filmschoolrejects.com/wp-content/uploads/2020/05/inception-top.jpg" alt="movie scene" blurRadius={blur} />
             </div>
         </div>
     )
