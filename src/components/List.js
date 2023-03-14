c08f8b4d7575fe12235ad0ffeaea1013c8d11cb6
Kajsia Anderson <kajsia.anderson@gmail.com>
2023-03-13 22:24:54 -0600

    testing if blur is causing problem

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { AppContext } from '../App'
 import Guess from './Guess'
-import Blur from "react-blur"
+// import Blur from "react-blur"
 
 const List = () => {
     const { currAttempt } = useContext(AppContext)
@@ -47,7 +47,7 @@ const List = () => {
             <div className='img'>
                 {/* <Blur img="https://filmschoolrejects.com/wp-content/uploads/2020/05/inception-top.jpg" alt="movie scene" blurRadius={blur} /> */}
                 {/* <Blur img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/12/28/movies_dark_knight_christian_bale_heath_ledger.jpg" alt="movie scene" blurRadius={blur} /> */}
-                <Blur img="https://www.gannett-cdn.com/-mm-/94572ad71c1c554abae670ec7bd4871e2898e3b2/c=0-142-3000-1837/local/-/media/2015/10/21/USATODAY/USATODAY/635809824922215243-AP-BACK-TO-THE-FUTURE-DAY-76890712.JPG?width=3000&height=1695&fit=crop&format=pjpg&auto=webp" alt="movie scene" blurRadius={blur} />
+                {/* <Blur img="https://www.gannett-cdn.com/-mm-/94572ad71c1c554abae670ec7bd4871e2898e3b2/c=0-142-3000-1837/local/-/media/2015/10/21/USATODAY/USATODAY/635809824922215243-AP-BACK-TO-THE-FUTURE-DAY-76890712.JPG?width=3000&height=1695&fit=crop&format=pjpg&auto=webp" alt="movie scene" blurRadius={blur} /> */}
             </div>
         </div>
     )
